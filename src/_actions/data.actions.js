244c2f86406e7f79cc463937b0fcac7798803257
a.pozdnyakov <a.pozdnyakov@itransition.com>
2018-10-17 18:13:48 +0300

    added createFakeServer

--- src/_actions/data.actions.js
+++ src/_actions/data.actions.js
@@ -3,36 +3,53 @@ import { createTasks } from '../_helpers/createTasks';
 
 export function getData() {
     return dispatch => {
-        dispatch({
-            type: rootConstants.get,
-            res: createTasks()
+        createFakeServer().then(() => {
+            dispatch({
+                type: rootConstants.get,
+                res: createTasks()
+            });
         });
     }
 }
 
 export function saveData(item) {
     return dispatch => {
-        dispatch({
-            type: rootConstants.save,
-            res: item
+        createFakeServer().then(() => {
+            alert('Changed successfully!');
+            dispatch({
+                type: rootConstants.save,
+                res: item
+            });
         });
     }
 }
 
 export function newData(item) {
     return dispatch => {
-        dispatch({
-            type: rootConstants.new,
-            res: item
+        createFakeServer().then(() => {
+            alert('Added successfully!');
+            dispatch({
+                type: rootConstants.new,
+                res: item
+            });
         });
     }
 }
 
 export function deleteItem(id) {
     return dispatch => {
-        dispatch({
-            type: rootConstants.delete,
-            res: id
+        createFakeServer().then(() => {
+            alert('Deleted successfully!');
+            dispatch({
+                type: rootConstants.delete,
+                res: id
+            });
         });
     }
+}
+
+function createFakeServer() {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve(), 1000)
+    });
 }
